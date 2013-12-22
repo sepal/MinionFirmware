3711c0a504371277321b207f6abc13cf2e3bcaa0
Sebastian Gilits <sep.gil@gmail.com>
2013-12-22 20:36:43 +0100

    Started working on sound initalizing prozedure.

--- MinionFirmware.ino
+++ MinionFirmware.ino
@@ -1,1 +1,24 @@
+#include <SPI.h>
+#include <SdFat.h>
+#include <SFEMP3Shield.h>
+#include <MemoryFree.h>
 
+const int FSR_THREASHOLD = 100;
+const int FSR_TIMER_THREASHOLD = 500;
+
+const int FSR_PIN = A0;
+const int EN_GPIO1 = A2;
+const int SD_CS = 9;
+
+// Files should be called [number].mp3
+char filename[5][6];
+long fsr_timer = 0;
+
+void setup () {
+  Serial.begin(9600);
+}
+
+
+
+void loop() {
+}
